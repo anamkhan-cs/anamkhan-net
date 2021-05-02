1af670000832fa96926a56a54b09a2469968a6c1
akhan-eb <akhan@eb.com>
2021-05-01 22:22:58 -0500

    fixed transition issue

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,4 +1,4 @@
-import { Container, Row, Col } from "reactstrap";
+import { Container } from "reactstrap";
 import {
   container,
   header,
@@ -14,7 +14,7 @@ const Experience = () => {
         <h1 className={header}>Education</h1>
       </div>
       <div className={contentContainer}>
-        <p>
+        <>
           <div className="d-flex justify-content-between align-items-center">
             <span> Master of Science</span>
             <span>
@@ -30,11 +30,9 @@ const Experience = () => {
               | Aug 2019 - Dec 2020{" "}
             </span>
           </div>
-          <div>
-            Computer Science |<span> GPA: 3.8</span>
-          </div>
-        </p>
-        <p>
+          <div className="ml-4">Computer Science - GPA: 3.8</div>
+        </>
+        <>
           <div className="d-flex justify-content-between align-items-center">
             <span>Bachelor of Engineering</span>
             <span>
@@ -50,10 +48,8 @@ const Experience = () => {
               | Aug 2015 - May 2019
             </span>
           </div>
-          <div>
-            Computer Science |<span> GPA: 3.5</span>
-          </div>
-        </p>
+          <div className="ml-4">Computer Science - GPA: 3.5</div>
+        </>
       </div>
       <div>
         <h1 className={header}>Experience</h1>
--- src/timelines/index.js
+++ src/timelines/index.js
@@ -1,26 +1,8 @@
-import { TimelineMax as Timeline, Power1 } from "gsap";
+import { TimelineMax as Timeline, Power1, gsap } from "gsap";
+import { CSSPlugin } from "gsap/CSSPlugin";
 
-const getDefaultTimeline = (node, delay) => {
-  const timeline = new Timeline({ paused: true });
-  const content = node.querySelector(".content");
-  const contentInner = node.querySelector(".content--inner");
-
-  timeline
-    .from(node, 0.3, {
-      display: "none",
-      autoAlpha: 0,
-      delay,
-      ease: Power1.easeIn,
-    })
-    .from(content, 0.15, { autoAlpha: 0, y: 25, ease: Power1.easeInOut })
-    .from(contentInner, 0.15, {
-      autoAlpha: 0,
-      delay: 0.15,
-      ease: Power1.easeIn,
-    });
-
-  return timeline;
-};
+// Force CSSPlugin to not get dropped during build
+gsap.registerPlugin(CSSPlugin);
 
 const getHomeTimeline = (node, delay) => {
   const timeline = new Timeline({ paused: true });
@@ -42,8 +24,7 @@ export const play = (pathname, node, appears) => {
   const delay = appears ? 0 : 0.5;
   let timeline;
 
-  if (pathname === "/") timeline = getHomeTimeline(node, delay);
-  else timeline = getDefaultTimeline(node, delay);
+  timeline = getHomeTimeline(node, delay);
 
   window.loadPromise.then(() => requestAnimationFrame(() => timeline.play()));
 };
