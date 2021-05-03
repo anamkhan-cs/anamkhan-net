44a2b22098f12e05a5a8ed668d5100797f34b3eb
akhan-eb <akhan@eb.com>
2021-05-03 00:13:36 -0500

    added work experience

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -13,7 +13,7 @@ import {
   GrFacebookOption,
   GrInstagram,
 } from "react-icons/gr";
-import { Container, Row } from "reactstrap";
+import { Container } from "reactstrap";
 import classnames from "classnames";
 
 import smoosh from "./smoosh.jpg";
@@ -45,56 +45,54 @@ const Contact = () => {
           <img src={smoosh} alt="My cat" className={picture} />
         </div>
       </Container>
-      <Row>
-        <footer className="footer">
-          <div
-            className={classnames(
-              "d-flex justify-content-center align-items-center",
-              iconContainer
-            )}
+      <footer className="footer">
+        <div
+          className={classnames(
+            "d-flex justify-content-center align-items-center",
+            iconContainer
+          )}
+        >
+          <a
+            href="mailto:agk997@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <a
-              href="mailto:agk997@gmail.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <GrMail className={icons} />
-            </a>
+            <GrMail className={icons} />
+          </a>
 
-            <a
-              href="https://www.linkedin.com/in/anamkhan-cs"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <GrLinkedin className={icons} />
-            </a>
+          <a
+            href="https://www.linkedin.com/in/anamkhan-cs"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <GrLinkedin className={icons} />
+          </a>
 
-            <a
-              href="https://github.com/anamkhan2"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <GrGithub className={icons} />
-            </a>
+          <a
+            href="https://github.com/anamkhan2"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <GrGithub className={icons} />
+          </a>
 
-            <a
-              href="https://www.facebook.com/anam.partyrockanthem/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <GrFacebookOption className={icons} />
-            </a>
+          <a
+            href="https://www.facebook.com/anam.partyrockanthem/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <GrFacebookOption className={icons} />
+          </a>
 
-            <a
-              href="https://www.instagram.com/anamgkhan/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <GrInstagram className={icons} />
-            </a>
-          </div>
-        </footer>
-      </Row>
+          <a
+            href="https://www.instagram.com/anamgkhan/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <GrInstagram className={icons} />
+          </a>
+        </div>
+      </footer>
     </>
   );
 };
--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -16,9 +16,8 @@ const Experience = () => {
       <div className={contentContainer}>
         <>
           <div className="d-flex justify-content-between align-items-center">
-            <span> Master of Science</span>
             <span>
-              |
+              Master of Science |
               <a
                 href="https://goo.gl/maps/7P2CoU66CrHgRoC36"
                 target="_blank"
@@ -27,16 +26,15 @@ const Experience = () => {
               >
                 <TiLocation /> Northwestern University
               </a>
-              | Aug 2019 - Dec 2020{" "}
             </span>
+            <span>| Aug 2019 - Dec 2020 </span>
           </div>
           <div className="ml-4">Computer Science - GPA: 3.8</div>
         </>
         <>
           <div className="d-flex justify-content-between align-items-center">
-            <span>Bachelor of Engineering</span>
             <span>
-              |
+              Bachelor of Engineering |
               <a
                 href="https://goo.gl/maps/Bq9Wvbcbp9BQGWmg8"
                 target="_blank"
@@ -45,14 +43,127 @@ const Experience = () => {
               >
                 <TiLocation /> Visvesvaraya Technological University
               </a>
-              | Aug 2015 - May 2019
             </span>
+            <span>| Aug 2015 - May 2019</span>
           </div>
           <div className="ml-4">Computer Science - GPA: 3.5</div>
         </>
+        <p className="mt-1">
+          Relevant Coursework: Agile Software Development, Programming in AI,
+          Machine Learning, Distributed Systems, Deep Learning, Computer
+          Networking, Human Computer Interaction, Algorithms, Data Structures,
+          Computer Architecture, Object Oriented Programming, Software
+          Engineering, Databases, Operating Systems
+        </p>
       </div>
+
       <div>
         <h1 className={header}>Experience</h1>
+      </div>
+      <div className={contentContainer}>
+        <div>
+          <a
+            href="https://goo.gl/maps/oTzfkad9vprLLqzb8"
+            target="_blank"
+            rel="noreferrer"
+            className={icons}
+          >
+            <TiLocation /> Encyclopedia Britannica, Inc
+          </a>
+          <div className="d-flex justify-content-between align-items-center">
+            <span>Software Engineer</span>
+            <span>| Jan 2021 - Present</span>
+          </div>
+          <div className="d-flex justify-content-between align-items-center">
+            <span>Software Engineer Intern</span>
+            <span>| Jun 2020 - Dec 2020</span>
+          </div>
+          <div>
+            <ul>
+              <li>
+                Front end enhancements, refactoring code, bug fixes on an
+                internal tool built with React, Bootstrap, Sass and testing with
+                Jest
+              </li>
+              <li>
+                Developed a stand alone, batch processing executable project
+                using Python, GUI using wxPython
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div>
+          <a
+            href="https://goo.gl/maps/7P2CoU66CrHgRoC36"
+            target="_blank"
+            rel="noreferrer"
+            className={icons}
+          >
+            <TiLocation /> Northwestern University
+          </a>
+          <div className="d-flex justify-content-between align-items-center">
+            <span>N’CAT Student Tutor</span>
+            <span>| Oct 2019 - Dec 2020</span>
+          </div>
+          <div>
+            <ul>
+              <li>Tutoring undergraduates courses in Computer Science</li>
+            </ul>
+          </div>
+        </div>
+
+        <div>
+          <a
+            href="https://goo.gl/maps/Pz9vwrBPEox5LmXn7"
+            target="_blank"
+            rel="noreferrer"
+            className={icons}
+          >
+            <TiLocation /> Old Dominion University
+          </a>
+          <div className="d-flex justify-content-between align-items-center">
+            <span>Software Development and Research Intern</span>
+            <span>| Jul 2018 - Aug 2018</span>
+          </div>
+          <div>
+            <ul>
+              <li>
+                Chatbot parsed user input and searched for homes Used Google
+                Dialogflow for NLP, Angular5/6 for frontend and Firebase as the
+                database. FAQs implemented by Algolia
+              </li>
+              <li>
+                To find methods of intrusion after detection of aggression
+                Worked with Emotiv, to analyze brain waves
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div>
+          <a
+            href="https://goo.gl/maps/NhYScvp8QnZPzqEZ6"
+            target="_blank"
+            rel="noreferrer"
+            className={icons}
+          >
+            <TiLocation /> Telaverge Communications
+          </a>
+          <div className="d-flex justify-content-between align-items-center">
+            <span>Front End Engineering Intern</span>
+            <span>| Apr 2018 - May 2018</span>
+          </div>
+          <div>
+            <ul>
+              <li>
+                Enhanced their scheduler to implement a to-do list and minutes
+                of meeting tool Worked with HTML/CSS, Javascript, Bootstrap with
+                MongoDB
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </Container>
   );
